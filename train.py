00d76734eee7e53f194875bd2a76e8ab7540f8e8
dhkdnduq <dhkdnduq@naver.com>
2021-11-15 09:06:08 +0900

    distance_matrix -> torch::cdist
    Improved performance

--- train.py
+++ train.py
@@ -189,20 +189,6 @@ def randomize_tensor(tensor):
     return tensor[torch.randperm(len(tensor))]
 
 
-def distance_matrix(x, y=None, p=2):  # pairwise distance of vectors
-
-    y = x if type(y) == type(None) else y
-
-    n = x.size(0)
-    m = y.size(0)
-    d = x.size(1)
-
-    x = x.unsqueeze(1).expand(n, m, d)
-    y = y.unsqueeze(0).expand(n, m, d)
-
-    dist = torch.pow(x - y, p).sum(2)
-
-    return dist
 
 
 class NN():
@@ -223,7 +209,8 @@ class NN():
             name = self.__class__.__name__
             raise RuntimeError(f"{name} wasn't trained. Need to execute {name}.train() first")
 
-        dist = distance_matrix(x, self.train_pts, self.p) ** (1 / self.p)
+        #dist = distance_matrix(x, self.train_pts, self.p) ** (1 / self.p)
+        dist = torch.cdist(x, self.train_pts, self.p)
         labels = torch.argmin(dist, dim=1)
         return self.train_label[labels]
 class KNN(NN):
@@ -240,8 +227,8 @@ class KNN(NN):
     def predict(self, x):
 
         test = 1 / self.p
-        dist = distance_matrix(x, self.train_pts, self.p) ** (1 / self.p)
-
+        #dist = distance_matrix(x, self.train_pts, self.p) ** (1 / self.p)
+        dist = torch.cdist(x, self.train_pts, self.p)
         knn = dist.topk(self.k, largest=False)
 
 
